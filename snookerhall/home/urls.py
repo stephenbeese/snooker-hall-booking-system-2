11e3e9e800372b0d1970b05ddf26e82a80406ecb
Stephen Beese <stephenbeese@stephens-air.home>
2024-07-11 16:46:57 +0100

    Fix dynamic styling bug

--- snookerhall/home/urls.py
+++ snookerhall/home/urls.py
@@ -5,9 +5,6 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("club_details_form/", views.club_details_view, name="club_details"),
     path("edit-club-details/", views.edit_club_details, name="edit_club_details"),
-    path(
-        "dynamic-styles.css", views.dynamic_styles, name="dynamic_styles"
-    ),  # URL for dynamic CSS
     path("opening-hours/", views.manage_opening_hours, name="manage_opening_hours"),
     path(
         "edit_opening_hours/<int:opening_hours_id>/",
