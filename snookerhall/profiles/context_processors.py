165b2dec072b9bc68a3f7a8e746133fcdc358317
stephenbeese <stephenbeese1@live.co.uk>
2025-05-19 12:50:58 +0100

    Fix profile creation logic errors

--- snookerhall/profiles/context_processors.py
+++ snookerhall/profiles/context_processors.py
@@ -1,15 +1,3 @@
-# from django.shortcuts import get_object_or_404
-# from .models import UserProfile
-
-
-# def profile_context_processors(request):
-#     profile = None
-#     if request.user.is_authenticated:
-#         profile = get_object_or_404(UserProfile, user=request.user)
-#     context = {"profile": profile}
-#     return context
-
-
 from .models import UserProfile
 
 
--- snookerhall/profiles/signals.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import UserProfile
-
-
-@receiver(user_logged_in)
-def ensure_profile_exists(sender, user, request, **kwargs):
-    UserProfile.objects.get_or_create(user=user)
